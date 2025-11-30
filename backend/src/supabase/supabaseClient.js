70b9aed57d5de7dbede82d1580241852bb6d4d0d
faiza-undip <faizatanjia@students.undip.ac.id>
2025-11-30 12:19:52 +0700

    Remove unused compiled files

--- backend/src/supabase/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import "dotenv/config";
-const supabaseUrl = process.env.SUPABASE_URL;
-const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-export const supabaseAdmin = createClient(supabaseUrl, serviceKey, {
-    auth: { persistSession: false },
-});
-//# sourceMappingURL=supabaseClient.js.map
